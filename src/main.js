2a62e2f53347c5f33c89ab69e8535f59d957717d
AnastasiiaKalashnikova <nastasyakalashnikova@icloud.com>
2023-10-05 22:30:30 +0200

    установки

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { Report } from 'notiflix/build/notiflix-report-aio';
+const axios = require('axios/dist/node/axios.cjs');
+
+
+const form = document.querySelector('.js-form')
+const gallery = document.querySelector('.js-gallery')
+const btn = document.querySelector('.js-btn')
